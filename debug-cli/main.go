591bc918696c6637de557bd4b89bfbeacbcdb5a5
vavilapallirahul <vavilapallirahul135@gmail.com>
2025-07-23 23:27:02 +0530

    modifying debug cmd and pod cmd

--- debug-cli/main.go
+++ debug-cli/main.go
@@ -1,7 +1,9 @@
 package debugcli
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -16,16 +18,34 @@ func Run_cmd(clientset *kubernetes.Clientset) {
 		Long:  `pto get pods name for debug purpose using logs`,
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			po_name := args[0]
+
+			fmt.Println(po_name)
+
 			ns, _ := cmd.Flags().GetString("namespace")
-			pods, err := clientset.CoreV1().Pods(ns).List(context.TODO(), metav1.ListOptions{})
+			_, err := clientset.CoreV1().Namespaces().Get(context.Background(), ns, metav1.GetOptions{})
+
+			if err != nil {
+				fmt.Printf("no namespcae with name %s , is there \n", ns)
+				panic(err.Error())
+			}
+
+			pod_info, err := clientset.CoreV1().Pods(ns).Get(context.Background(), po_name, metav1.GetOptions{})
 			if err != nil {
 				panic(err.Error())
 			}
 
-			for _, val := range pods.Items {
-				fmt.Println("pods name : ", val.Name)
-				// fmt.Println("total spec : ", val)
+			pod_status_byte, err := json.Marshal(pod_info.Status)
+
+			if err != nil {
+				err.Error()
 			}
+
+			var prettyJSON bytes.Buffer
+			json.Indent(&prettyJSON, pod_status_byte, "", "  ")
+
+			fmt.Println(prettyJSON.String())
+
 		},
 	}
 
